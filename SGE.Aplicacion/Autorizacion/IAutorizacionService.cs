de1d85fdb0e684e43ce101588f2931399b1a88ea
Eneas Cisneros <101111243+CisnerosEneas@users.noreply.github.com>
2026-05-18 02:00:52 -0300

    Add files via upload

--- SGE.Aplicacion/Autorizacion/IAutorizacionService.cs
+++ SGE.Aplicacion/Autorizacion/IAutorizacionService.cs
@@ -3,5 +3,5 @@ namespace SGE.Aplicacion.Autorizacion;
 
 public interface IAutorizacionService
 {
-    bool PoseeElPermiso(Guid idUsuario,Permiso permiso);
+    bool PoseeElPermiso(Guid idUsuario, Permiso permiso);
 }
