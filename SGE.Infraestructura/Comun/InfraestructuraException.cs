76e62089a4f9ea75ba6aae255ed64194b66231b7
Eneas Cisneros <101111243+CisnerosEneas@users.noreply.github.com>
2026-06-25 19:21:55 -0300

    Delete SGE.Infraestructura directory

--- SGE.Infraestructura/Comun/InfraestructuraException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace SGE.Infraestructura.Comun;
-
-public class InfraestructuraException : Exception
-{
-    public InfraestructuraException(string mensaje) : base(mensaje)
-    {
-    }
-}
